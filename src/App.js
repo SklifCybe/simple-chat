3e69788719d934fa3d7bfb1353269fc76958013b
SklifCybe <i.s.toaccept@gmail.com>
2021-08-27 21:20:53 +0300

    create layout

--- src/App.js
+++ src/App.js
@@ -2,7 +2,38 @@ import React from 'react';
 
 function App() {
   return (
-    <div>hello</div>
+    <div className="container">
+      <div className="formEnter">
+        <input type="text" placeholder="Номер комнаты" className="enterFields" />
+        <input type="text" placeholder="Ваше имя" className="enterFields" />
+        <button className="btnEnter">Войти</button>
+      </div>
+      {/* <div className="content">
+        <div className="sideBar">
+          <span className="titleUsers">Users(1):</span>
+          <ul>
+            <li className="items">Test User</li>
+          </ul>
+        </div>
+        <div className="chat">
+          <ul>
+            <li className="items">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <span>Test User</span>
+            </li>
+            <li className="items">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <span>Test User</span>
+            </li>
+          </ul>
+          <div className="form">
+            <div className="bottomLine"></div>
+            <textarea></textarea>
+            <button>Send</button>
+          </div>
+        </div>
+      </div> */}
+    </div>
   );
 }
 
